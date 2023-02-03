d09934f6c8dfbdac443247de7e43a4823671b5f8
Lucas  Rodrigues Estefano <42548142+lucasg13info@users.noreply.github.com>
2023-02-02 23:12:53 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,39 +1,16 @@
-// Modelo 1
-// let numeroUm = 1;
-// let numeroDois = 2;
-
-// let result = numeroUm + numeroDois
-// console.log("O resultado da soma é: ", result)
-
-
-//Modelo 2:
-// let numero1 = prompt("Informa o primeiro número: ")
-// let numero2 = prompt("Informe o número dois: ");
-
-// let numeroNew1 = parseInt(numero1)
-// let numeroNew2 = parseInt(numero2)
-
-// var resultant = parseInt(numeroNew1 + numeroNew2)
-
-// alert("O resultado da soma é: " + resultant)
-
-
-
-// Modelo 3 - Calculadora IMC
+// Calculadora IMC
 console.log("Calculadora IMC")
 
 let peso = prompt("Informe qual seu peso (KG)")
 let pesoNumber = parseFloat(peso)
-console.log(pesoNumber)
+//console.log(pesoNumber)
 
 let altura = prompt("Informe sua altura (Cm ex: 180)")
 let alturaNumber = parseFloat(altura /100)
-console.log(alturaNumber)
-
+//console.log(alturaNumber)
 
 let calcImc = pesoNumber / (alturaNumber * alturaNumber)
-console.log("Resultado",calcImc)
-
+//console.log("Resultado",calcImc)
 
 
 if (calcImc < 17){
